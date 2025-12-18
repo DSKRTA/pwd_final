5e4846909d3af37b2b7d835cb59af319f6da496a
DSKRTA <kimsung0114@ajou.ac.kr>
2025-12-18 18:05:40 +0900

    correction 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['VITE_', 'SUPABASE_'], // Allow SUPABASE_ prefix variables
   base: './', // Use relative paths for assets - required for live-server
   server: {
     port: 3002,
